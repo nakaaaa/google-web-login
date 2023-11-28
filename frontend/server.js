8a3059c2febb4cf3caa376fdb124634e5c94bf86
nakaaaa <nkazuya6073@gmail.com>
2023-11-29 00:47:46 +0900

    add custom server

---
+++ frontend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // CORSを許可するミドルウェアを追加
+  server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+  // Next.jsアプリケーションへの接続
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('Server is running on port 3000');
+  });
+});
